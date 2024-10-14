d7816bbc49b669c8cafa45583fca0f8f337974ed
mervezgin <mervezgin@gmail.com>
2024-10-14 17:09:55 +0300

    refactored the playercontroller script to new input system

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        /*if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }*/
+        inputVector = inputVector.normalized;
+        return inputVector;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,43 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private GameInput gameInput;
     private float moveSpeed = 3.0f;
+    private float rotateSpeed = 10.0f;
     private bool isWalking;
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +0.1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -0.1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -0.1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +0.1f;
-        }
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
 
-        float rotateSpeed = 10.0f;
         isWalking = moveDirection != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
-        Debug.Log(inputVector);
-        Debug.Log(moveDirection);
     }
 
-    public bool IsWalking()
-    {
-        return isWalking;
-    }
+    public bool IsWalking() { return isWalking; }
 }
