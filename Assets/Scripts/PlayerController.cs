a25e5d0551011fd5d9e7d509c5305babd9baf015
mervezgin <mervezgin@gmail.com>
2024-10-14 11:53:27 +0300

    fixed move and rotation problems

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float moveSpeed = 3.0f;
-    private float rotateSpeed = 10.0f;
+    private bool isWalking;
 
     // Update is called once per frame
     void Update()
@@ -13,24 +13,34 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W))
         {
-            inputVector.y = +1f;
+            inputVector.y = +0.1f;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            inputVector.y = -1f;
+            inputVector.y = -0.1f;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            inputVector.x = -1f;
+            inputVector.x = -0.1f;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            inputVector.x = +1f;
+            inputVector.x = +0.1f;
         }
         inputVector = inputVector.normalized;
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float rotateSpeed = 10.0f;
+        isWalking = moveDirection != Vector3.zero;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+        Debug.Log(inputVector);
+        Debug.Log(moveDirection);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
