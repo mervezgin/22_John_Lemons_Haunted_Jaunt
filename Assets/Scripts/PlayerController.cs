02cd957be7131a706efd3b1525a406bb8fa24367
mervezgin <mervezgin@gmail.com>
2024-10-14 10:22:24 +0300

    added playercontroller.cs script to player
    -move the character properly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float moveSpeed = 3.0f;
+    private float rotateSpeed = 10.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1f;
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
     }
 }
